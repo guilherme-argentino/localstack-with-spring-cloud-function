5f5b58b0e63ab8ec4ab31927543b421dafc87fcc
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2022-11-05 23:04:33 -0300

    chore: Define an Application class

---
+++ src/main/kotlin/org/localstack/sampleproject/Application.kt
@@ -0,0 +1,10 @@
+package org.localstack.sampleproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    // Do nothing unless you use a custom runtime
+}
